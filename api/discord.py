d20f34b39c83f7dd841af38cac78376865dc7e78
user <treact123@gmail.com>
2026-04-16 12:41:11 +0500

    endpoint add bot

--- api/discord.py
+++ api/discord.py
@@ -20,7 +20,7 @@ async def endpoint_discord_servers(request: Request) -> Response:
 
 
 @router.post('/api/v1/discord/add-server')
-async def endpoint_discord_servers(request: Request) -> Response:
+async def endpoint_discord_add_server(request: Request) -> Response:
     pool = request.app.state.pool
     data = await request.json()
     name = data.get("name")
@@ -59,3 +59,35 @@ async def endpoint_discord_bots(request: Request) -> Response:
             return JSONResponse(status_code=404, content={"message": "No servers found"})
 
     return JSONResponse(content=[dict(row) for row in rows])
+
+
+@router.post('/api/v1/discord/add-bot')
+async def endpoint_discord_add_bot(request: Request) -> Response:
+    pool = request.app.state.pool
+    data = await request.json()
+    name = data.get("name")
+    description = data.get("description")
+    img_url = data.get("img_url")
+    
+    print("Данные с фронта:", data)
+
+    if not name:
+        return JSONResponse(status_code=404, content={"message": "Not found name"})
+
+    async with pool.acquire() as conn:
+        await conn.execute(
+            '''INSERT INTO discord_bots (name, img_url, description) VALUES 
+                ($1, $2, $3)
+            ''',
+            name,
+            img_url,
+            description
+        )
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "success",
+            "data": dict({"name": name, "img_url": img_url, "description": description})
+        }
+    )
